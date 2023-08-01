811d95a8f602a573ec910df880f2653528e8fd64
Maciek Miazek <matat99@Macieks-MacBook-Pro.local>
2023-08-01 03:44:59 +0100

    slowly figuring it out

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import yfinance as yf
 import pandas as pd
 import json
+from datetime import date,timedelta
 
 # Load tickers from JSON file
 with open('tickers.json', 'r') as file:
     tickers_dict = json.load(file)
 
-# Get the list of tickers
-tickers_list = list(tickers_dict.values())
+# Load purchase dates from JSON file
+with open('purchase_date.json', 'r') as file:
+    purchase_date_dict = json.load(file)
 
 # Load investments from JSON file
 with open('investment.json', 'r') as file:
