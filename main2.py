730d18b0e939e826ca0b550ef20b97061b9441d6
Maciek Miazek <matat99@Macieks-MacBook-Pro.local>
2023-08-04 03:12:09 +0100

    another save point

--- main2.py
+++ main2.py
@@ -20,97 +20,31 @@ with open('transactions.json', 'r') as file:
 # Create an empty portfolio dictionary 
 portfolio_dict = {}
 
-# Record Transactions
 
-def get_price_on_date(ticker, date_str):
-    """
-    Get the closing price of a given stock ticker on a specific date.
-
-    Parameters:
-    ticker (str): The ticker symbol of the stock.
-    date_str (str): The date to get the price for, in 'YYYY-MM-DD' format.
-
-    Returns:
-    float: The closing price on the specified date. 
-           Returns 'No data' if no price data is available for the date.
-    """
-    # Convert date string to datetime
-    check_date = pd.to_datetime(date_str)
-
-    # Set a date range around the target date
-    start_date = (check_date - pd.DateOffset(days=1)).strftime('%Y-%m-%d')
-    end_date = (check_date + pd.DateOffset(days=1)).strftime('%Y-%m-%d')
-
-    try:
-        # Fetch data for the date range
-        data = yf.download(ticker, start=start_date, end=end_date, progress=False)
-
-        # Extract data for the target date
-        if check_date in data.index:
-            return data.loc[check_date, 'Close']
-        else:
-            return 'No data'
-    except Exception as e:
-        return f'Error: {e}'
-
-
-# Populate the portfolio dictionary
-for ticker, transactions in transactions_dict.items():
-    # Skip if there are no transactions for the ticker
-    if not transactions:
-        continue
-
-    # Sort transactions by date
-    transactions_sorted = sorted(transactions, key=lambda k: pd.to_datetime(k['date']))
-
-    # Get the first transaction
-    first_transaction = transactions_sorted[0]
-
-    # Fetch the price on the date of the first transaction
-    price = get_price_on_date(ticker, first_transaction['date'])
-
-    # Add the ticker to the portfolio dictionary with the value, price, and date
-    portfolio_dict[ticker] = {
-        'value': first_transaction['amount'],
-        'price': price,
-        'date': first_transaction['date'],
-    }
 
 
 def get_weekly_performance(tickers_dict):
-    # Create a dictionary to store the performance for each ticker
     performance_dict = {}
-
-    # Get today's date and one week ago date in the required format
     today = pd.Timestamp.now(tz='UTC').floor('D') - pd.DateOffset(days=1)
     one_week_ago = today - pd.DateOffset(weeks=1)
-
-    # Convert to string format that yf.download understands
     today_str = today.strftime('%Y-%m-%d')
     one_week_ago_str = one_week_ago.strftime('%Y-%m-%d')
 
-    # Iterate through each ticker
     for name, ticker in tickers_dict.items():
         try:
-            # Get the data for the past week
             data = yf.download(ticker, start=one_week_ago_str, end=today_str, progress=False)
             
             if data.empty:
                 performance_dict[name] = 'No data'
                 continue
 
-            # Get the last available close price
             close_price = data['Close'][-1]
             close_date = data.index[-1]
 
-            # Get the close price from a week ago
             week_ago_price = data['Close'][0]
             week_ago_date = data.index[0]
 
-            # Calculate the percentage change
             percentage_change = ((close_price - week_ago_price) / week_ago_price) * 100
-
-            # Store the percentage change in the performance dictionary
             performance_dict[name] = round(percentage_change, 2)
 
             print(f"{name} \nWeek Ago Date: {week_ago_date}, Week Ago Price: {week_ago_price}\nClose Date: {close_date}, Close Price: {close_price}\n")
@@ -138,43 +72,25 @@ with open('weekly_performance.txt','w') as file:
 
 
 def get_position_worth(tickers_dict, transactions_dict):
-    # Create a dictionary to store the worth of each position
     position_worth_dict = {}
 
-    # Iterate through each ticker
     for name, ticker in tickers_dict.items():
         try:
-            # Get the transactions for the current ticker
             transactions = transactions_dict.get(ticker, [])
-
-            # Get the historical data for the current ticker
             data = yf.download(ticker, progress=False)
-
-            # Initialize a variable to store the total worth of the position
             total_worth = 0.0
 
-            # Iterate through each transaction
             for transaction in transactions:
-                # Get the date and amount of the transaction
                 transaction_date = pd.to_datetime(transaction['date'])
                 transaction_amount = transaction['amount']
 
-                # Get the close price on the date of the transaction
                 transaction_price = data.loc[transaction_date, 'Close']
-
-                # Calculate the number of shares bought
                 shares_bought = transaction_amount / transaction_price
 
-                # Get the last available close price
                 close_price = data['Close'][-1]
-
-                # Calculate the current worth of the shares bought in the transaction
                 worth = shares_bought * close_price
-
-                # Add the worth of the current transaction to the total worth
                 total_worth += worth
 
-            # Store the total worth in the position worth dictionary
             position_worth_dict[name] = total_worth
 
         except Exception as e:
