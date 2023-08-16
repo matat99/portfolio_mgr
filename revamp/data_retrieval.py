c782a55f1c7a84f6e0ff5f616f1882823fbf3fc3
matat99 <mrmatat99@gmail.com>
2023-08-16 21:20:29 +0100

    done kinda

--- revamp/data_retrieval.py
+++ revamp/data_retrieval.py
@@ -1,102 +1,187 @@
 # data_retrieval.py
 
+import requests
 import yfinance as yf
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
 
-def calculate_position_values(transactions_dict, current_tickers, downloaded_data):
+api_key = "42c83d3d0b0e24c532ce1cd511d95724" # They key is hard-coded... I know it's bad practice FUCK YOU 
+
+def get_eur_exchange_rates(api_key):
+    """Fetch exchange rates for 1 EUR to other currencies."""
+    target_currencies = ["GBP", "USD"]
+    url = f"http://api.exchangeratesapi.io/latest?base=EUR&symbols={','.join(target_currencies)}&access_key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data['rates']
+
+
+def calculate_position_values_with_currency_adjustment(transactions_dict, current_tickers, downloaded_data, eur_rates):
     position_values = {}
+    total_portfolio_value = 0
 
-    # Only process tickers that are currently in the portfolio
-    for ticker in current_tickers.values():
+    # Fetch EUR exchange rates to other major currencies
+    eur_rates = get_eur_exchange_rates(api_key)
+
+    currency_mapping = {
+        '': 'USD',     # Default to USD for NASDAQ and similar
+        '.L': 'GBP',
+        '.DE': 'EUR',
+        '.PA': 'EUR',
+        '.F': 'EUR'   # Frankfurt Stock Exchange
+    }
+
+    for name, ticker in current_tickers.items():
         transactions = transactions_dict.get(ticker, [])
-
-        # If there are no transactions for a ticker, its value is 0
+        
+        # If no transactions, move on.
         if not transactions:
-            position_values[ticker] = 0.0
             continue
 
         try:
             data = downloaded_data[ticker]
 
+            # Determine the stock's currency using the mapping
+            suffix = ticker.split('.')[-1] if '.' in ticker else ''
+            currency = currency_mapping.get('.' + suffix, 'USD')
+
             total_shares = 0.0
             for transaction in transactions:
                 transaction_date = pd.to_datetime(transaction['date'])
-                
-                # Ensure the date is within the available data range
                 if transaction_date not in data.index:
-                    print(f"Date {transaction_date} not available for {ticker}. Skipping this transaction.")
                     continue
                 
-                print(f"Using data for {ticker} on {transaction_date}.")
-                
-                if transaction['amount'] > 0:  # Buying
+                if transaction['amount'] > 0:
                     shares_bought = transaction['amount'] / data.loc[transaction_date, 'Close']
                     total_shares += shares_bought
-                else:  # Selling
+                else:
                     shares_sold = -transaction['amount'] / data.loc[transaction_date, 'Close']
                     total_shares -= shares_sold
 
-            # Calculate the current value of the position based on current stock price and total shares
-            position_values[ticker] = total_shares * data['Close'].iloc[-1]
+            current_value_in_stock_currency = total_shares * data['Close'].iloc[-1]
+            
+            # Convert to GBP using the EUR as a pivot
+            if currency != 'GBP':
+                to_eur_rate = 1 / eur_rates[currency] if currency in eur_rates else 1  # If EUR, this rate will be 1
+                current_value_in_eur = current_value_in_stock_currency * to_eur_rate
+                current_value_in_gbp = current_value_in_eur * eur_rates['GBP']
+            else:
+                current_value_in_gbp = current_value_in_stock_currency
+
+            position_values[name] = current_value_in_gbp
+            total_portfolio_value += current_value_in_gbp
 
         except Exception as e:
-            position_values[ticker] = f"Error: {e}"
+            position_values[name] = f"Error: {e}"
 
+    position_values['Total Portfolio'] = total_portfolio_value
     return position_values
 
 
-def weekly_performance(data_dict, current_tickers, filename='weekly.csv'):
-    """
-    Calculate the weekly performance for a list of tickers based on their data.
-    
-    :param data_dict: Dictionary containing tickers as keys and their data as values.
-    :param current_tickers: Dictionary of currently held tickers.
-    :return: Sorted list of tickers based on their weekly performance.
-    """
-    performance = {}
 
-    for name, ticker in current_tickers.items():
-        if ticker not in data_dict:
-            print(f"No data available for {name} ({ticker}). Skipping.")
+def weekly_performance(transactions_dict, data_dict, name_to_ticker_map):
+    weekly_performance_dict = {}
+
+    end_value = 0.0
+    start_value = 0.0
+
+    for name, ticker in name_to_ticker_map.items():
+        transactions = transactions_dict.get(ticker, [])
+        if not transactions:
             continue
 
-        data = data_dict[ticker]
-        
-        try:
-            # Extract the last available close price
-            last_close = data['Close'].iloc[-1]
-            
-            # Try to get the close price from exactly 7 days earlier
-            # If not available, skip to the closest available previous date
-            one_week_ago_date = data.index[-1] - pd.Timedelta(days=7)
-            one_week_ago_data = data[data.index <= one_week_ago_date].tail(1)
+        data = data_dict.get(ticker)
+        if data is None:
+            print(f"Data not available for {ticker}. Skipping...")
+            continue
 
-            # If there's no data from a week ago, skip this ticker
-            if one_week_ago_data.empty:
-                print(f"Insufficient data for {name} ({ticker}) to compute weekly performance.")
+        total_shares = 0.0
+        for transaction in transactions:
+            transaction_date = pd.to_datetime(transaction['date'])
+            if transaction_date not in data.index:
                 continue
+                
+            if transaction['amount'] > 0:
+                shares_bought = transaction['amount'] / data.loc[transaction_date, 'Close']
+                total_shares += shares_bought
+            else:
+                shares_sold = -transaction['amount'] / data.loc[transaction_date, 'Close']
+                total_shares -= shares_sold
 
-            one_week_ago_close = one_week_ago_data['Close'].iloc[0]
-            
-            # Calculate percentage change
-            change = ((last_close - one_week_ago_close) / one_week_ago_close) * 100
-            formatted_change = f"{change:.2f}%"
-            performance[name] = formatted_change
+        recent_price = data['Close'].iloc[-1]
 
-        except Exception as e:
-            print(f"Error computing weekly performance for {name} ({ticker}): {e}")
+        # Getting the close from 5 trading days ago
+        one_week_ago_index = -6 if len(data) >= 6 else -len(data)
+        one_week_ago_price = data['Close'].iloc[one_week_ago_index]
 
-    # Sort tickers based on performance
-    sorted_performance = dict(sorted(performance.items(), key=lambda item: item[1], reverse=True))
-    
-    # saving to csv
-    df = pd.DataFrame(sorted_performance.items(), columns=['Company Name', 'Weekly Performance'])
-    df.to_csv(filename, index=False)
+        start_value += total_shares * one_week_ago_price
+        end_value += total_shares * recent_price
+
+        stock_weekly_performance = ((recent_price - one_week_ago_price) / one_week_ago_price) * 100
+        weekly_performance_dict[name] = round(stock_weekly_performance, 2)
+
+    portfolio_weekly_performance = ((end_value - start_value) / start_value) * 100
+    weekly_performance_dict['Total Portfolio'] = round(portfolio_weekly_performance, 2)
+
+    df = pd.DataFrame(weekly_performance_dict.items(), columns=['Company Name', 'Weekly Performance (%)'])
+    df.to_csv('weekly_performance.csv', index=False)
+    return df
+
+
+def yearly_performance(transactions_dict, data_dict, name_to_ticker_map):
+    yearly_performance_dict = {}
+
+    end_value = 0.0
+    start_value = 0.0
+
+    for name, ticker in name_to_ticker_map.items():
+        transactions = transactions_dict.get(ticker, [])
+        if not transactions:
+            continue
+
+        data = data_dict.get(ticker)
+        if data is None:
+            print(f"Data not available for {ticker}. Skipping...")
+            continue
+
+        total_shares = 0.0
+        for transaction in transactions:
+            transaction_date = pd.to_datetime(transaction['date'])
+            if transaction_date not in data.index:
+                continue
+                
+            if transaction['amount'] > 0:
+                shares_bought = transaction['amount'] / data.loc[transaction_date, 'Close']
+                total_shares += shares_bought
+            else:
+                shares_sold = -transaction['amount'] / data.loc[transaction_date, 'Close']
+                total_shares -= shares_sold
+
+        recent_price = data['Close'].iloc[-1]
+
+        # Getting the close from approximately 252 trading days ago (assuming 252 trading days in a year)
+        one_year_ago_index = -253 if len(data) >= 253 else -len(data)
+        one_year_ago_price = data['Close'].iloc[one_year_ago_index]
+
+        start_value += total_shares * one_year_ago_price
+        end_value += total_shares * recent_price
+
+        stock_yearly_performance = ((recent_price - one_year_ago_price) / one_year_ago_price) * 100
+        yearly_performance_dict[name] = round(stock_yearly_performance, 2)
+
+    portfolio_yearly_performance = ((end_value - start_value) / start_value) * 100
+    yearly_performance_dict['Total Portfolio'] = round(portfolio_yearly_performance, 2)
+
+    df = pd.DataFrame(yearly_performance_dict.items(), columns=['Company Name', 'Yearly Performance (%)'])
+    df.to_csv('yearly_performance.csv', index=False)
+    return df
 
 
 
-def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_map):
+
+
+
+def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_map, current_portfolio_value):
     performance_dict = {}
 
     for name, ticker in name_to_ticker_map.items():
@@ -120,13 +205,19 @@ def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_m
         purchase_price = data.loc[first_transaction_date, 'Close']
         recent_price = data['Close'].iloc[-1]
 
-        # Calculate percentage change
+        # Calculate percentage change for the stock
         percentage_change = ((recent_price - purchase_price) / purchase_price) * 100
         performance_dict[name] = round(percentage_change, 2)
+
+    # Calculate overall performance of the fund
+    starting_fund_value = 10000
+    fund_percentage_change = ((current_portfolio_value - starting_fund_value) / starting_fund_value) * 100
+    performance_dict["Overall Fund Performance"] = round(fund_percentage_change, 2)
 
     # Convert dictionary to DataFrame and save to CSV
     df = pd.DataFrame(performance_dict.items(), columns=['Company Name', 'Performance (%)'])
     df.to_csv('overall_performance.csv', index=False)
     return df
+
 
 
--- revamp/main.py
+++ revamp/main.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import os
 
 from utilities import load_dict_from_json, save_to_csv
-from data_retrieval import calculate_position_values, weekly_performance, calculate_overall_performance
+from data_retrieval import weekly_performance, calculate_overall_performance, get_eur_exchange_rates, calculate_position_values_with_currency_adjustment, yearly_performance
 from data_download import get_earliest_transaction_year, download_data_for_tickers
 
+api_key = "42c83d3d0b0e24c532ce1cd511d95724" # They key is hard-coded... I know it's bad practice FUCK YOU 
 
 if __name__ == "__main__":
 
@@ -18,19 +19,23 @@ if __name__ == "__main__":
     
     print(type(transaction_data))
 
-    # Download all the data
+    # Download all the data for tickers and exchange rates
 
     downloaded_data = download_data_for_tickers(transaction_data)
+    eur_rates = get_eur_exchange_rates(api_key)
 
     # Value of our current positions 
+    # Cash position is currently hard-coded in the save_to_csv function (Not long term hopefully)
 
-    value = calculate_position_values(transaction_data, current_tickers, downloaded_data)
+    value = calculate_position_values_with_currency_adjustment(transaction_data, current_tickers, downloaded_data, eur_rates)
 
-    save_to_csv(value, current_tickers)
+    save_to_csv(value)
 
-    weekly = weekly_performance(downloaded_data, current_tickers)
+    weekly = weekly_performance(transaction_data, downloaded_data, current_tickers)
 
-    overall = calculate_overall_performance(transaction_data, downloaded_data, current_tickers)
+    yearly = yearly_performance(transaction_data, downloaded_data, current_tickers)
+
+    overall = calculate_overall_performance(transaction_data, downloaded_data, current_tickers, value['Total Portfolio'])
 
     print(overall)
     
