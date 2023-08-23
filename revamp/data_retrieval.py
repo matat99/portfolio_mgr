7edd155fafa06b9ecc3c21a149e42b603af2c952
matat99 <mrmatat99@gmail.com>
2023-08-23 10:04:11 +0100

    added some extra stuff that I'll have to document later and those commit messages will be SUPER unhelpful but oh well lmao

--- revamp/data_retrieval.py
+++ revamp/data_retrieval.py
@@ -4,12 +4,13 @@ import requests
 import yfinance as yf
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
+import json
 
 api_key = "42c83d3d0b0e24c532ce1cd511d95724" # They key is hard-coded... I know it's bad practice FUCK YOU 
 
 def get_eur_exchange_rates(api_key):
     """Fetch exchange rates for 1 EUR to other currencies."""
-    target_currencies = ["GBP", "USD"]
+    target_currencies = ["GBP", "USD","CAD"]
     url = f"http://api.exchangeratesapi.io/latest?base=EUR&symbols={','.join(target_currencies)}&access_key={api_key}"
     response = requests.get(url)
     data = response.json()
@@ -28,7 +29,7 @@ def calculate_position_values_with_currency_adjustment(transactions_dict, curren
         '.L': 'GBP',
         '.DE': 'EUR',
         '.PA': 'EUR',
-        '.F': 'EUR'   # Frankfurt Stock Exchange
+        '.TO': 'CAD'   # Toronto Stock Exchange
     }
 
     for name, ticker in current_tickers.items():
@@ -58,7 +59,7 @@ def calculate_position_values_with_currency_adjustment(transactions_dict, curren
                     shares_sold = -transaction['amount'] / data.loc[transaction_date, 'Close']
                     total_shares -= shares_sold
 
-            current_value_in_stock_currency = total_shares * data['Close'].iloc[-1]
+            current_value_in_stock_currency = total_shares * data['Close'].loc["2023-05-31"] #<- specific cutoff | data['Close'].iloc[-1] <- last close date
             
             # Convert to GBP using the EUR as a pivot
             if currency != 'GBP':
@@ -76,7 +77,6 @@ def calculate_position_values_with_currency_adjustment(transactions_dict, curren
 
     position_values['Total Portfolio'] = total_portfolio_value
     return position_values
-
 
 
 def weekly_performance(transactions_dict, data_dict, name_to_ticker_map):
@@ -236,6 +236,8 @@ def yearly_performance_YoY(transactions_dict, data_dict, name_to_ticker_map):
 def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_map, current_portfolio_value):
     performance_dict = {}
 
+    cutoff_date = pd.to_datetime("2023-05-31")
+
     for name, ticker in name_to_ticker_map.items():
         # Extract transactions for the ticker
         transactions = transactions_dict.get(ticker, [])
@@ -255,7 +257,8 @@ def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_m
 
         # Get close price on purchase date and the most recent close price
         purchase_price = data.loc[first_transaction_date, 'Close']
-        recent_price = data['Close'].iloc[-1]
+        #recent_price = data['Close'].iloc[-1] # <- This is for last close
+        recent_price = data.loc[cutoff_date, 'Close']
 
         # Calculate percentage change for the stock
         percentage_change = ((recent_price - purchase_price) / purchase_price) * 100
@@ -270,9 +273,6 @@ def calculate_overall_performance(transactions_dict, data_dict, name_to_ticker_m
     df = pd.DataFrame(performance_dict.items(), columns=['Company Name', 'Performance (%)'])
     df.to_csv('overall_performance.csv', index=False)
     return df
-
-
-
 
 
 def monthly_portfolio_performance(transactions_dict, data_dict, name_to_ticker_map):
@@ -325,3 +325,91 @@ def monthly_portfolio_performance(transactions_dict, data_dict, name_to_ticker_m
     df.to_csv('monthly_portfolio_performance.csv', index=False)
 
     return df
+
+
+
+def get_close_prices_on_cutoff(data_dict, tickers_dict):
+    cutoff_date = pd.to_datetime("2023-05-31")
+    close_prices = {}
+
+    for name, ticker in tickers_dict.items():
+        ticker_data = data_dict.get(ticker)
+        
+        if ticker_data is not None and cutoff_date in ticker_data.index:
+            close_prices[name] = ticker_data.loc[cutoff_date, 'Close']
+        else:
+            close_prices[name] = None
+
+    return close_prices
+
+
+def get_portfolio_summary(transactions_dict, current_tickers, downloaded_data, eur_rates):
+    cutoff_date = pd.to_datetime("2023-05-31")
+    summary = {}
+    total_portfolio_value = 0
+
+    currency_mapping = {
+        '': 'USD',
+        '.L': 'GBP',
+        '.DE': 'EUR',
+        '.PA': 'EUR',
+        '.F': 'EUR'
+    }
+
+    for name, ticker in current_tickers.items():
+        transactions = transactions_dict.get(ticker, [])
+        
+        # If no transactions, move on.
+        if not transactions:
+            continue
+
+        try:
+            data = downloaded_data[ticker]
+            close_price_on_cutoff = data.loc[cutoff_date, 'Close'] if cutoff_date in data.index else None
+
+            # Determine the stock's currency using the mapping
+            suffix = ticker.split('.')[-1] if '.' in ticker else ''
+            currency = currency_mapping.get('.' + suffix, 'USD')
+
+            total_shares = 0.0
+            for transaction in transactions:
+                transaction_date = pd.to_datetime(transaction['date'])
+                if transaction_date not in data.index:
+                    continue
+                
+                if transaction['amount'] > 0:
+                    shares_bought = transaction['amount'] / data.loc[transaction_date, 'Close']
+                    total_shares += shares_bought
+                else:
+                    shares_sold = -transaction['amount'] / data.loc[transaction_date, 'Close']
+                    total_shares -= shares_sold
+
+            current_value_in_stock_currency = total_shares * close_price_on_cutoff
+            
+            # Convert to GBP using the EUR as a pivot
+            if currency != 'GBP':
+                to_eur_rate = 1 / eur_rates[currency] if currency in eur_rates else 1
+                current_value_in_eur = current_value_in_stock_currency * to_eur_rate
+                current_value_in_gbp = current_value_in_eur * eur_rates['GBP']
+            else:
+                current_value_in_gbp = current_value_in_stock_currency
+
+            total_portfolio_value += current_value_in_gbp
+
+            summary[name] = {
+                'Shares Held': total_shares,
+                'Value in GBP': current_value_in_gbp,
+                'Close Price on 2023-05-31': close_price_on_cutoff
+            }
+
+        except Exception as e:
+            summary[name] = f"Error: {e}"
+
+    summary['Total Portfolio'] = {'Value in GBP': total_portfolio_value}
+
+    with open("portfolio_summary.json", "w") as f:
+        json.dump(summary, f, indent=4)
+
+    return summary
+
+
--- revamp/main.py
+++ revamp/main.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import os
 
 from utilities import load_dict_from_json, save_to_csv
-from data_retrieval import weekly_performance, calculate_overall_performance, get_eur_exchange_rates, calculate_position_values_with_currency_adjustment, yearly_performance_June2June, yearly_performance_YoY, monthly_portfolio_performance
-from data_download import get_earliest_transaction_year, download_data_for_tickers
+from data_retrieval import weekly_performance, calculate_overall_performance, get_eur_exchange_rates, calculate_position_values_with_currency_adjustment, yearly_performance_June2June, yearly_performance_YoY, monthly_portfolio_performance, get_close_prices_on_cutoff, get_portfolio_summary
+from data_download import download_data_for_tickers, load_saved_data
 
 api_key = "42c83d3d0b0e24c532ce1cd511d95724" # They key is hard-coded... I know it's bad practice FUCK YOU 
 
@@ -21,23 +21,31 @@ if __name__ == "__main__":
 
     # Download all the data for tickers and exchange rates
 
-    downloaded_data = download_data_for_tickers(transaction_data)
+    #downloaded_data = download_data_for_tickers(transaction_data)
+    downloaded_data = load_saved_data() # uses data downloaded onto the computer so not hit the api everytime when debugging
     eur_rates = get_eur_exchange_rates(api_key)
 
     # Value of our current positions 
     # Cash position is currently hard-coded in the save_to_csv function (Not long term hopefully)
 
+    
     value = calculate_position_values_with_currency_adjustment(transaction_data, current_tickers, downloaded_data, eur_rates)
 
     save_to_csv(value)
 
-    weekly = weekly_performance(transaction_data, downloaded_data, current_tickers)
+    #weekly = weekly_performance(transaction_data, downloaded_data, current_tickers)
 
-    yearly = yearly_performance_YoY(transaction_data, downloaded_data, current_tickers)
+    #yearly = yearly_performance_YoY(transaction_data, downloaded_data, current_tickers)
 
     overall = calculate_overall_performance(transaction_data, downloaded_data, current_tickers, value['Total Portfolio'])
 
-    graph = monthly_portfolio_performance(transaction_data, downloaded_data, current_tickers)
+    J2J = yearly_performance_June2June(transaction_data, downloaded_data, current_tickers)
 
-    print(graph)
+    #close = get_close_prices_on_cutoff(downloaded_data, current_tickers)
+
+    #summary = get_portfolio_summary(transaction_data, current_tickers, downloaded_data, eur_rates)
+
+    #graph = monthly_portfolio_performance(transaction_data, downloaded_data, current_tickers)
+
+    print(J2J)
     
