464a1c638dcdf904fb4e76ec3b51abb8f800e71b
matat99 <mrmatat99@gmail.com>
2023-08-23 10:04:11 +0100

    added some extra stuff that I'll have to document later and those commit messages will be SUPER unhelpful but oh well lmao

--- revamp/data_download.py
+++ revamp/data_download.py
@@ -1,23 +1,17 @@
 import yfinance as yf
+import time
 import pandas as pd
-import time 
+import pickle
 
-def get_earliest_transaction_year(transactions_dict):
-    years = {}
-    for ticker, transactions in transactions_dict.items():
-        if transactions:  # Check if transactions list is not empty
-            dates = [pd.to_datetime(transaction['date']) for transaction in transactions]
-            earliest_year = min(dates).year
-            years[ticker] = earliest_year
-    return years
-
-
-def download_data_for_tickers(tickers, retries=3, delay=5):
+def download_data_for_tickers(tickers, retries=3, delay=5, save_to_file=True):
     data_dict = {}
+    start_date = pd.to_datetime("2017-01-01")
+    end_date = pd.to_datetime("today")  # To get data up to today
+
     for ticker in tickers:
         for i in range(retries):
             try:
-                data = yf.download(ticker, progress=False)
+                data = yf.download(ticker, start=start_date, end=end_date, progress=False)
                 data_dict[ticker] = data
                 break
             except Exception as e:
@@ -28,4 +22,15 @@ def download_data_for_tickers(tickers, retries=3, delay=5):
                 else:
                     print(f"Failed to download data for {ticker} after {retries} retries.")
         time.sleep(1)  # Introduce a 1-second delay between requests
+
+    # Save to file if flag is set
+    if save_to_file:
+        with open("downloaded_data.pkl", "wb") as f:
+            pickle.dump(data_dict, f)
+
+    return data_dict
+
+def load_saved_data(file_name="downloaded_data.pkl"):
+    with open(file_name, "rb") as f:
+        data_dict = pickle.load(f)
     return data_dict
