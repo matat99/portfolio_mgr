576193afcc576b76a1f8d6b79eef25b6203620f9
matat99 <mrmatat99@gmail.com>
2023-11-01 13:51:37 +0000

    re link with big pc

--- revamp/main.py
+++ revamp/main.py
@@ -8,8 +8,8 @@ import pandas as pd
 import csv
 
 from utilities import load_dict_from_json, save_to_csv
-from data_retrieval import weekly_performance, calculate_overall_performance, get_eur_exchange_rates, calculate_position_values_with_currency_adjustment, yearly_performance_June2June, yearly_performance_YoY, weekly_portfolio_performance_with_currency_adjustment, calculate_total_portfolio_value_as_of_date
-from data_download import download_data_for_tickers, load_saved_data, download_exchange_rates, load_saved_exchange_rates
+from data_retrieval import weekly_performance, calculate_overall_performance, get_eur_exchange_rates, calculate_position_values_with_currency_adjustment, yearly_performance_June2June, yearly_performance_YoY, weekly_portfolio_performance_with_currency_adjustment, calculate_total_portfolio_value_as_of_date, weekly_portfolio_values
+from data_download import download_data_for_tickers, load_saved_data, download_weekly_exchange_rates, load_saved_exchange_rates
 
 api_key = "42c83d3d0b0e24c532ce1cd511d95724" # They key is hard-coded... I know it's bad practice FUCK YOU 
 
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     # Download all the data for tickers and exchange rates
 
-    downloaded_fx = load_saved_exchange_rates()
-    #downloaded_fx = download_exchange_rates(api_key)
+    #downloaded_fx = load_saved_exchange_rates()
+    downloaded_fx = download_weekly_exchange_rates(api_key)
     #downloaded_data = download_data_for_tickers(transaction_data)
     downloaded_data = load_saved_data() # uses data downloaded onto the computer so not hit the api everytime when debugging
     #eur_rates = get_eur_exchange_rates(api_key)
@@ -40,6 +40,8 @@ if __name__ == "__main__":
     #value = calculate_position_values_with_currency_adjustment(transaction_data, current_tickers, downloaded_data)
 
     #save_to_csv(value)
+
+    weekly_something = weekly_portfolio_values(transaction_data, downloaded_data, api_key)
 
     #weekly = weekly_performance(transaction_data, downloaded_data, current_tickers)
 
@@ -65,43 +67,44 @@ if __name__ == "__main__":
 
     #weekly_value = calculate_total_portfolio_value_as_of_date(transaction_data, downloaded_data, downloaded_fx, date="2022-05-31")
 
-    #print(weekly_value)
+    print(weekly_something)
+    #print(value)
 
 
-    # Your function to generate weekly dates
-    def generate_weekly_dates(cutoff_date, end_date="2023-05-31"):
-        weekly_dates = []
+    # # Your function to generate weekly dates
+    # def generate_weekly_dates(cutoff_date, end_date="2023-05-31"):
+    #     weekly_dates = []
         
-        # Convert the dates to datetime objects for manipulation
-        cutoff_date = datetime.strptime(cutoff_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    #     # Convert the dates to datetime objects for manipulation
+    #     cutoff_date = datetime.strptime(cutoff_date, "%Y-%m-%d")
+    #     end_date = datetime.strptime(end_date, "%Y-%m-%d")
         
-        # Generate weekly dates from end_date backwards to cutoff_date
-        current_date = end_date
-        while current_date >= cutoff_date:
-            weekly_dates.append(current_date.strftime("%Y-%m-%d"))
-            current_date -= timedelta(days=7)
+    #     # Generate weekly dates from end_date backwards to cutoff_date
+    #     current_date = end_date
+    #     while current_date >= cutoff_date:
+    #         weekly_dates.append(current_date.strftime("%Y-%m-%d"))
+    #         current_date -= timedelta(days=7)
         
-        return weekly_dates
+    #     return weekly_dates
 
-    # Generate weekly dates from 2022-05-17 to 2023-05-31
-    weekly_dates = generate_weekly_dates("2017-12-18")
+    # # Generate weekly dates from 2022-05-17 to 2023-05-31
+    # weekly_dates = generate_weekly_dates("2017-12-18")
 
-    # Dictionary to hold the total portfolio value for each date
-    total_portfolio_values = {}
+    # # Dictionary to hold the total portfolio value for each date
+    # total_portfolio_values = {}
 
-    # Loop over each weekly date and calculate the total portfolio value
-    for date in weekly_dates:
-        total_value = calculate_total_portfolio_value_as_of_date(transaction_data, downloaded_data, downloaded_fx, date=date)
-        total_portfolio_values[date] = total_value
+    # # Loop over each weekly date and calculate the total portfolio value
+    # for date in weekly_dates:
+    #     total_value = calculate_total_portfolio_value_as_of_date(transaction_data, downloaded_data, downloaded_fx, date=date)
+    #     total_portfolio_values[date] = total_value
 
-    with open('total_portfolio_values.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Date', 'Total Portfolio Value']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for date, value in total_portfolio_values.items():
-            writer.writerow({'Date': date, 'Total Portfolio Value': value})
+    # with open('total_portfolio_values.csv', 'w', newline='') as csvfile:
+    #     fieldnames = ['Date', 'Total Portfolio Value']
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for date, value in total_portfolio_values.items():
+    #         writer.writerow({'Date': date, 'Total Portfolio Value': value})
 
-    print(type(transaction_data))
-    print(downloaded_data['ESGD'].head())
-    print(type(downloaded_data['ESGD'].index[0]))
+    # print(type(transaction_data))
+    # print(downloaded_data['ESGD'].head())
+    # print(type(downloaded_data['ESGD'].index[0]))
